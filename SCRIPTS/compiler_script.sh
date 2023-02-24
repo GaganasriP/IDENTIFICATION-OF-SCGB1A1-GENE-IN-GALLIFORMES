d97fc1271245ee40f43ff2097555501c509c6b71
GaganasriP <71751796+GaganasriP@users.noreply.github.com>
2023-02-25 00:24:15 +0530

    Update compiler_script.sh

--- SCRIPTS/compiler_script.sh
+++ SCRIPTS/compiler_script.sh
@@ -4,7 +4,7 @@ for i in `grep ">" APOC1_ORFout_gbgc.fasta_PRANK.aln |sed 's/>//g'`
 do 
 for m in bneutral  
 do
-for cf in F3x4 F1x4
+for cf in `echo F3x4 F1x4`
 do
 x=`echo "$i"_"$cf"_"$m"`
 ombnfg=`grep "(dN/dS)" $x.out|awk '{print $6}'`
@@ -17,7 +17,7 @@ done
 done
 for m in bfree
 do
-for cf in F3x4 F1x4
+for cf in `echo F3x4 F1x4` 
 do
 y=`echo "$i"_"$cf"_"$m"`
 ombffg=`grep "(dN/dS)" $y.out|awk '{print $6}'`
@@ -28,7 +28,7 @@ done
 done
 for m in M0
 do
-for cf in F3x4 F1x4
+for cf in `echo F3x4 F1x4`
 do
 z=`echo Chiroptera_"$cf"_"$m"`       
 mdl=`grep "Codon frequency model:" $z.out|awk -F ":" '{print $2}'|awk '{print $1}'`
